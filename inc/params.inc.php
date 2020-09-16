62b3befab1cbfb01f9392481026ffef4bd7bdebe
ShariaTelecom <shariatelecom@gmail.com>
2020-09-16 18:57:12 +0700

    modifying params.inc.php

--- inc/params.inc.php
+++ inc/params.inc.php
@@ -88,15 +88,15 @@ define('BX_ACCOUNT_CONFIRMATION_PHONE', 'phone');
 define('BX_ACCOUNT_CONFIRMATION_EMAIL_PHONE', 'email_and_phone');
 
 //--- Include files needed for basic functionality ---//
-require_once(BX_DIRECTORY_PATH_CLASSES . 'BxDol.php');
-require_once(BX_DIRECTORY_PATH_INC . 'utils.inc.php');
-require_once(BX_DIRECTORY_PATH_INC . 'version.inc.php');
+require_once('/srv/app/naval/inc/classes/BxDol.php');
+require_once('/srv/app/naval/inc/utils.inc.php');
+require_once('/srv/app/naval/inc/version.inc.php');
 
 spl_autoload_register('bx_autoload');
 
 BxDolDb::getInstance()->cacheParams();
 
-require_once(BX_DIRECTORY_PATH_INC . 'profiles.inc.php');
+require_once('/srv/app/naval/inc/profiles.inc.php');
 
 $o = new BxDolAlerts('system', 'begin', 0);
 $o->alert();
